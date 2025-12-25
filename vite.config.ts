8268fedde9439a6d45fc5af2121c911a822e991a
v_lqinggliu <v_lqinggliu@tencent.com>
2025-12-25 17:21:57 +0800

    打包淡旺季

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './',
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
-      plugins: [],
+      plugins: [viteSingleFile()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
